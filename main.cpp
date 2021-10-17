2d454f8907240a712fd2c198b032ca985f070ece
da-nie <da-nie@yandex.ru>
2021-10-17 12:50:02 +0300

    Сделана синхронизация через мультимедийный таймер Windows.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,17 @@ static const double FPS=33;//частота кадров
 HINSTANCE hProjectInstance;
 CWnd_Main cWnd_Main;
 
+LARGE_INTEGER start_time;
+
+//****************************************************************************************************
+//прототипы функций
+//****************************************************************************************************
+void CALLBACK TimerEvent(UINT uTimerID,UINT uMsg,DWORD_PTR dwUser,DWORD_PTR dw1,DWORD_PTR dw2);//функция мультимедийного таймера
+
+//****************************************************************************************************
+//реализация функций
+//****************************************************************************************************
+
 //----------------------------------------------------------------------------------------------------
 //главная функция программы
 //----------------------------------------------------------------------------------------------------
@@ -37,6 +48,18 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevstance,LPSTR lpstrCmdLine,
  //SetWindowPos(hWndS,HWND_TOPMOST,0,0,0,SWP_NOMOVE|SWP_NOREDRAW,SWP_NOSIZE);
 
  timeBeginPeriod(1);
+ MMRESULT res=timeSetEvent(1000/FPS,1,TimerEvent,0,TIME_PERIODIC);
+ if (res!=NULL)
+ {
+  while(GetMessage(&msg,NULL,0,0))
+  {
+   TranslateMessage(&msg);
+   DispatchMessage(&msg);
+  }
+  timeKillEvent(res);
+ }
+ else MessageBox(NULL,"Не удалось создать мультимедийный таймер!","Ошибка",MB_OK);
+ /*
  DWORD start_time=GetTickCount();
  while(1)
  {
@@ -63,6 +86,7 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevstance,LPSTR lpstrCmdLine,
   start_time=GetTickCount();
   cWnd_Main.Processing();
  }
+ */
  timeEndPeriod(1);
 
  /*
@@ -115,4 +139,41 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevstance,LPSTR lpstrCmdLine,
  */
  //while(ShowCursor(TRUE)==FALSE);
  return(msg.wParam);
+}
+
+//----------------------------------------------------------------------------------------------------
+//функция мультимедийного таймера
+//----------------------------------------------------------------------------------------------------
+void CALLBACK TimerEvent(UINT uTimerID,UINT uMsg,DWORD_PTR dwUser,DWORD_PTR dw1,DWORD_PTR dw2)
+{
+ LARGE_INTEGER current_time;
+ QueryPerformanceCounter(&current_time);
+ cWnd_Main.Processing();
+
+ /*
+ LARGE_INTEGER CounterFrequency;
+ QueryPerformanceFrequency(&CounterFrequency);
+ double d_CounterFrequency=(double)CounterFrequency.QuadPart;
+ double delta_time=(double)(current_time.QuadPart-start_time.QuadPart);
+ delta_time/=d_CounterFrequency;
+ delta_time*=1000.0;
+
+ static int time_array[256];
+ static int pos=0;
+
+ time_array[pos]=delta_time;
+ pos++;
+
+ if (pos==256)
+ {
+  FILE *file=fopen("time.txt","ab");
+  for(size_t n=0;n<pos;n++)
+  {
+   fprintf(file,"delta:%i\r\n",time_array[n]);
+  }
+  fclose(file);
+  pos=0;
+ }
+ */
+ start_time=current_time;
 }
